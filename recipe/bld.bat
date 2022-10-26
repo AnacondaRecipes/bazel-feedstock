9fbbc3f728a7f9f793b5a9761f1d3d5fabcb06d6
ArishaMays1 <1arishamays1@gmail.com>
2022-10-26 12:14:50 -0400

    testing amendments to build.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -27,7 +27,7 @@ set MSYS2_PATH_TYPE=inherit
 set CHERE_INVOKING=1
 set "BAZEL_VC=%VSINSTALLDIR%VC"
 set "BAZEL_VS=%VSINSTALLDIR%"
-bash -lc "%SRC_DIR%"/compile.sh
+bash -lx ./compile.sh
 if errorlevel 1 exit 1
 
 copy %saved_source_dir%\output\bazel.exe %LIBRARY_BIN%\
