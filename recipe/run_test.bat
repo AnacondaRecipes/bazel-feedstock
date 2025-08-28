4ee63902c10e7f35f40cf5cafc412eade9ce1b85
Brian Wing <bwing@anaconda.com>
2025-08-28 15:40:31 -0400

    Update to v7.6.1

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,4 +1,10 @@
-bazel -h
-if errorlevel 1 exit 1
+@echo on
 
-exit 0
+mkdir tutorial
+ROBOCOPY %RECIPE_DIR%\tutorial tutorial /E
+cd tutorial
+set BAZEL_BUILD_OPTS=""
+set "EXTRA_BAZEL_ARGS=--host_javabase=@local_jdk//:jdk"
+set "BAZEL_VS=%VSINSTALLDIR%"
+
+bazel build //main:hello-world
