b35a592ff140e6468ed3349dd0a0ff8e1c4d6e10
Eric Lundby <Eric.Lundby@gmail.com>
2025-11-26 10:07:00 -0700

    Shutdown before running tests.

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -6,7 +6,8 @@ set -exuo pipefail
 cp -r ${RECIPE_DIR}/tutorial .
 cd tutorial
 source gen-bazel-toolchain
-bazel build --logging=6 --subcommands --verbose_failures //main:hello-world \
+bazel shutdown
+bazel --batch build --logging=6 --subcommands --verbose_failures //main:hello-world \
   --platforms=//bazel_toolchain:target_platform \
   --host_platform=//bazel_toolchain:build_platform \
   --extra_toolchains=//bazel_toolchain:cc_cf_toolchain \
