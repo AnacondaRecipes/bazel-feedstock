5f685e073e337d229f1a08abae7ed6835f5656f7
Eric Lundby <Eric.Lundby@gmail.com>
2026-08-17 13:48:26 -0600

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -81,8 +81,9 @@ if marker in text:
 PY
 
 cp -ap $PREFIX/share/bazel/protobuf/bazel third_party/systemlibs/protobuf/
-# bazel/upb_*.bzl forward to //upb/bazel:*, which grpc's build rules load
-cp -ap $PREFIX/share/bazel/protobuf/upb third_party/systemlibs/protobuf/
+# bazel/upb_*.bzl forward to //upb/bazel:*, which grpc's build rules load.
+# protobuf-bazel-rules installs upb at share/bazel/upb/, not share/bazel/protobuf/upb/.
+cp -ap $PREFIX/share/bazel/upb third_party/systemlibs/protobuf/
 
 ./compile.sh
 
